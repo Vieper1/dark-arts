3c062d9258ff2e62081fd15ecdcd6f09efc3376f
naiduvishal13 <naiduvishal13@gmail.com>
2020-05-20 22:29:37 -0600

    Added post processing to camera TUT1&2

---
+++ Assets/Scripts/Gameplay/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    [Header("Timer")]
+    public float GoldTime = 6f;
+    public float SilverTime = 12f;
+    public float BronzeTime = 18f;
+    public float GameTime = 0f;
+
+
+
+
+
+    void Start()
+    {
+        if (Instance == null) Instance = this;
+        else if (Instance != this) Destroy(gameObject);
+        GameTime = 0;
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
